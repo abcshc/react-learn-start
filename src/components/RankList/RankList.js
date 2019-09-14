834b64dd1fc6c8f1902d32c4854a8198a7715c5d
HeeChul So <thrrkthrrk@gmail.com>
2019-09-14 14:31:42 +0900

    Add react for test

---
+++ src/components/RankList/RankList.js
@@ -0,0 +1,135 @@
+import React from 'react';
+
+class RankList extends React.Component {
+    render() {
+        return (
+            <div class="rank_inner" data-age="all">
+                <strong class="rank_title v2">전체 연령대</strong>
+                <ul class="rank_list v2">
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">1</em>
+                            <span class="title">트루 시크릿</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">2</em>
+                            <span class="title">잼라이브</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">3</em>
+                            <span class="title">유노윤호</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">4</em>
+                            <span class="title">피터래빗</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">5</em>
+                            <span class="title">인스턴트 패밀리</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">6</em>
+                            <span class="title">조지환</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">7</em>
+                            <span class="title">쥬만지</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">8</em>
+                            <span class="title">아이를 위한 나라는 있다 싱글대디</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">9</em>
+                            <span class="title">레플리카</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">10</em>
+                            <span class="title">김홍영 검사</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">11</em>
+                            <span class="title">타워하이스트</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">12</em>
+                            <span class="title">아이를 위한 나라는 있다</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">13</em>
+                            <span class="title">송해</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">14</em>
+                            <span class="title">양자물리학</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">15</em>
+                            <span class="title">잼라이브 오늘의 힌트</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">16</em>
+                            <span class="title">조혜련</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">17</em>
+                            <span class="title">쥬만지 새로운세계</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">18</em>
+                            <span class="title">광대들 풍문조작단</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">19</em>
+                            <span class="title">남주혁</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="#" class="list_area">
+                            <em class="num">20</em>
+                            <span class="title">잼라이브 힌트</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default RankList;
